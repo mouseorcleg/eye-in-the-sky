9bfdd8fb74753087f6bc940a58d262d5ca352836
mouseorcleg <torindubozit@gmail.com>
2022-11-08 16:56:20 +0000

    MVVM structure -  basic files

---
+++ Eye in the sky/Models.swift
@@ -0,0 +1,8 @@
+//
+//  Models.swift
+//  Eye in the sky
+//
+//  Created by Maria Kharybina on 08/11/2022.
+//
+
+import Foundation
---
+++ Eye in the sky/ViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  ViewModel.swift
+//  Eye in the sky
+//
+//  Created by Maria Kharybina on 08/11/2022.
+//
+
+import Foundation
