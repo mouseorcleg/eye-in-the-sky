4ffb679cd846e57ae89a9610f0a7befc53e43eb9
mouseorcleg <torindubozit@gmail.com>
2022-11-08 14:57:07 +0000

    Initial Commit

---
+++ Eye in the sky/Eye_in_the_skyApp.swift
@@ -0,0 +1,17 @@
+//
+//  Eye_in_the_skyApp.swift
+//  Eye in the sky
+//
+//  Created by Maria Kharybina on 08/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct Eye_in_the_skyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
