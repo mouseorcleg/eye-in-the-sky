594646ce3bf9fa09ff793a76cc3a596a389ccb15
mouseorcleg <torindubozit@gmail.com>
2022-11-11 11:39:47 +0000

    added a Model for data from API call

--- Eye in the sky/Models.swift
+++ Eye in the sky/Models.swift
@@ -6,3 +6,20 @@
 //
 
 import Foundation
+
+// Data
+
+struct WeatherDataModel: Codable {
+    let timezone: Float
+    let name: String
+    let weather: CurrentWeather
+    let main: MainDetails
+}
+
+struct CurrentWeather: Codable {
+    let description: String
+}
+
+struct MainDetails: Codable {
+    let temp: Float
+}
