2eca94dd7dfa1639c54b721781ef9fd642d734e4
mouseorcleg <torindubozit@gmail.com>
2022-11-12 16:20:54 +0000

    Separated the cell view

--- Eye in the sky/WeatherListView.swift
+++ Eye in the sky/WeatherListView.swift
@@ -18,30 +18,7 @@ struct WeatherListView: View {
                 
                 //fetches data from a viewModel
                 
-                HStack {
-                    VStack (alignment: .center){
-                        Text(viewModel.title)
-                            .font(.title)
-                            .fontDesign(.monospaced)
-                            .fontWeight(.semibold)
-                        AsyncImage(url: URL(string: "https://openweathermap.org/img/wn/\(viewModel.icon).png"), scale: 0.5)
-                            .scaledToFit()
-                    }
-                    VStack (alignment: .leading) {
-                        Text(viewModel.temp)
-                            .font(.largeTitle)
-                            .fontWeight(.bold)
-                            .fontDesign(.monospaced)
-                        Text(viewModel.description)
-                            .font(.title2)
-                            .fontDesign(.monospaced)
-                        Text(viewModel.humidity)
-                            .fontDesign(.monospaced)
-                        Text(viewModel.wind)
-                            .fontDesign(.monospaced)
-                    }
-                    .padding(40)
-                }
+                
             }
             .navigationTitle("Whether the weather")
             .navigationBarTitleDisplayMode(.automatic)
