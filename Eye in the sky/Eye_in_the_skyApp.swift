828999940846d14c840b704c7933868221812560
mouseorcleg <torindubozit@gmail.com>
2022-12-08 15:53:34 +0000

    gave SwiftUI access to the database

--- Eye in the sky/Eye_in_the_skyApp.swift
+++ Eye in the sky/Eye_in_the_skyApp.swift
@@ -12,7 +12,25 @@ struct Eye_in_the_skyApp: App {
     
     var body: some Scene {
         WindowGroup {
-            WeatherListView().environmentObject(WeatherRepository(weatherService: WeatherService()))
+            WeatherListView()
+                .environmentObject(WeatherRepository(weatherService: WeatherService()))
+                .environment(\.weatherDatabase, .shared)
         }
+    }
+}
+
+// MARK: - Give SwiftUI access to the database
+// Define a new environment key that grants access to an WeatherDatabase.
+// The technique is documented at
+// <https://developer.apple.com/documentation/swiftui/environmentkey>.
+
+private struct WeatherDatabaseKey: EnvironmentKey {
+    static var defaultValue: WeatherDatabase { .empty() }
+}
+
+extension EnvironmentValues {
+    var weatherDatabase: WeatherDatabase {
+        get { self[WeatherDatabaseKey.self] }
+        set { self[WeatherDatabaseKey.self] = newValue }
     }
 }
